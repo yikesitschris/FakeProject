81ae3b67e5584345305a1eb133cafebd8d836db8
JungHwan Park <sam.junghwan@gmail.com>
2016-11-28 20:23:59 -0800

    Changed chris code

--- src/FakeHello.java
+++ src/FakeHello.java
@@ -2,5 +2,7 @@
 public class FakeHello {
 	public static void main(String[] args) {
 		System.out.println("Sam is mean");
+		System.out.println("Sam is cool not mean");
+		System.out.println("Chris also likes man");
 	}
 }
